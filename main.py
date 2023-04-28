7580d6913de3c8f7e805923162d37d151ef1af1b
JosephR <joseph_rundell@outlook.com>
2023-04-28 02:42:24 -0400

    Switched from pytube to yt-dlp

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from pytube import YouTube
+# imports
+from tkinter import filedialog
+from tkinter import *
+from yt_dlp import YoutubeDL
 
-def Download(link):
-    youtubeObject = YouTube(link)  
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
-    try:
-        youtubeObject.download()
-    except:
-        print("An error has occured")
-    print("Download is complete")
+def submit():
+    URLS = entry.get()
+    print(URLS)
+    with YoutubeDL() as ydl:
+        ydl.download(URLS)
 
-link = input("Enter the youtube url:")
+window = Tk()
+entry = Entry()
 
-Download(link)
+# set window resolution
+window.geometry("500x200")
+
+folderSelected = filedialog.askdirectory()
+
+# textEntry widget
+entry.insert(0, "Paste Youtube Link")
+entry.pack()
+
+# submitButton widget
+submit = Button(window, text="submit", command=submit)
+submit.pack(side = BOTTOM)
+
+# starts window
+window.mainloop()
